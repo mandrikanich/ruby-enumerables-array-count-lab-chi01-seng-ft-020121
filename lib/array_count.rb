661367e924542eeaaf65ebcfeea332daf899a7a9
Michael Andrikanich <mandrikanich@gmail.com>
2021-01-12 18:18:34 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -4,7 +4,7 @@ def count_strings(array)
   #expect(count_strings(array_1)).to eq(3)
   #expect(count_strings(array_2)).to eq(4)
   #["", "hello", "world", 4, 1, 5, [], {}]
-  binding.pry
+  element.is_a?String
 end 
 end
 
