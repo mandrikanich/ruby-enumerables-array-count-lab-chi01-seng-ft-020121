5ba375bad79d4a07fe8f94ac86fe88e8d9662bd1
Michael Andrikanich <mandrikanich@gmail.com>
2021-01-12 18:09:53 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,5 +1,11 @@
 def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
+  array.count do 
+  # Return the total number of strings in the provided #array using the count enumerable
+  #expect(count_strings(array_1)).to eq(3)
+  #expect(count_strings(array_2)).to eq(4)
+  #["", "hello", "world", 4, 1, 5, [], {}]
+  #binding.pry
+  
 end
 
 def count_empty_strings(array)
